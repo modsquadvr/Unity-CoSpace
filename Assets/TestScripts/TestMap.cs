6e5dabaf35563cb737afa7019f224659a82dffbb
Matthew Plaudis <matthew.plaudis@mail.utoronto.ca>
2019-10-09 13:10:43 -0400

    Unity Slippy Map Project (Esri tile service)
    Current Unity project, using Esri mapping services to create the tile layers.
    
    A substantial amount of ode from the original Slippy Map project has been deleted and repurposed for our project.

---
+++ Assets/TestScripts/TestMap.cs
@@ -0,0 +1,249 @@
+// 
+//  TestMap.cs
+//  
+//  Author:
+//       Jonathan Derrough <jonathan.derrough@gmail.com>
+//  
+//  Copyright (c) 2012 Jonathan Derrough
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// #define DEBUG_LOG
+
+using UnityEngine;
+
+using System;
+
+using UnitySlippyMap.Map;
+using UnitySlippyMap.Markers;
+using UnitySlippyMap.Layers;
+using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
+using ProjNet.Converters.WellKnownText;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.UI;
+
+public class TestMap : MonoBehaviour
+{
+
+    //public Button myZoomButton;
+
+    private MapBehaviour		map;
+	
+	public Texture	LocationTexture;
+	public Texture	MarkerTexture;
+
+	
+	private float	guiXScale;
+	private float	guiYScale;
+	private Rect	guiRect;
+	
+	private bool 	isPerspectiveView = false;
+	private float	perspectiveAngle = 30.0f;
+	private float	destinationAngle = 0.0f;
+	private float	currentAngle = 0.0f;
+	private float	animationDuration = 0.5f;
+	private float	animationStartTime = 0.0f;
+
+
+
+    private List<LayerBehaviour> layers;
+    private int     currentLayerIndex = 0;
+
+    private string utfGridJsonString = "";
+
+	bool Toolbar(MapBehaviour map)
+	{
+		GUI.matrix = Matrix4x4.Scale(new Vector3(guiXScale, guiXScale, 1.0f));
+		
+		GUILayout.BeginArea(guiRect);
+		
+		GUILayout.BeginHorizontal();
+		
+		GUILayout.Label("Zoom: " + map.CurrentZoom);
+		
+		bool pressed = false;
+
+        /*myZoomButton.onClick.AddListener(ZoomIn);
+        void ZoomIn()
+        {
+            Debug.Log("You clicked Zoom.");
+        }
+        */
+
+        if (GUILayout.RepeatButton("+", GUILayout.ExpandHeight(true)))
+		{
+			map.Zoom(1.0f);
+			pressed = true;
+		}
+        if (Event.current.type == EventType.Repaint)
+        {
+            Rect rect = GUILayoutUtility.GetLastRect();
+            if (rect.Contains(Event.current.mousePosition))
+                pressed = true;
+        }
+
+        if (GUILayout.Button("2D/3D", GUILayout.ExpandHeight(true)))
+		{
+			if (isPerspectiveView)
+			{
+				destinationAngle = -perspectiveAngle;
+			}
+			else
+			{
+				destinationAngle = perspectiveAngle;
+			}
+			
+			animationStartTime = Time.time;
+			
+			isPerspectiveView = !isPerspectiveView;
+		}
+        if (Event.current.type == EventType.Repaint)
+        {
+            Rect rect = GUILayoutUtility.GetLastRect();
+            if (rect.Contains(Event.current.mousePosition))
+                pressed = true;
+        }
+
+      
+        if (Event.current.type == EventType.Repaint)
+        {
+            Rect rect = GUILayoutUtility.GetLastRect();
+            if (rect.Contains(Event.current.mousePosition))
+                pressed = true;
+        }
+
+        string layerMessage = String.Empty;
+        if (map.CurrentZoom > layers[currentLayerIndex].MaxZoom)
+            layerMessage = "\nZoom out!";
+        else if (map.CurrentZoom < layers[currentLayerIndex].MinZoom)
+            layerMessage = "\nZoom in!";
+        if (GUILayout.Button(((layers != null && currentLayerIndex < layers.Count) ? layers[currentLayerIndex].name + layerMessage : "Layer"), GUILayout.ExpandHeight(true)))
+        {
+            ++currentLayerIndex;
+            if (currentLayerIndex >= layers.Count)
+            {
+                currentLayerIndex = 0;
+            }
+
+            map.IsDirty = true;
+        }
+
+        if (GUILayout.RepeatButton("-", GUILayout.ExpandHeight(true)))
+		{
+			map.Zoom(-1.0f);
+			pressed = true;
+		}
+        if (Event.current.type == EventType.Repaint)
+        {
+            Rect rect = GUILayoutUtility.GetLastRect();
+            if (rect.Contains(Event.current.mousePosition))
+                pressed = true;
+        }
+		
+		GUILayout.EndHorizontal();
+					
+		GUILayout.EndArea();
+
+        // Show any mbtiles utf string under the mouse position
+        if (!string.IsNullOrEmpty(utfGridJsonString))
+            GUILayout.Label(utfGridJsonString);
+
+		return pressed;
+
+	}
+	
+	private IEnumerator Start()
+	{
+
+        //myZoomButton = GameObject.Find("Button_zoom_+").GetComponent<Button>();
+
+        // setup the gui scale according to the screen resolution
+        guiXScale = (Screen.orientation == ScreenOrientation.Landscape ? Screen.width : Screen.height) / 480.0f;
+        guiYScale = (Screen.orientation == ScreenOrientation.Landscape ? Screen.height : Screen.width) / 640.0f;
+		// setup the gui area
+		guiRect = new Rect(16.0f * guiXScale, 4.0f * guiXScale, Screen.width / guiXScale - 32.0f * guiXScale, 32.0f * guiYScale);
+
+		// create the map singleton
+		map = MapBehaviour.Instance;
+		map.CurrentCamera = Camera.main;
+
+        // **MAY HAVE TO CHANGE THIS TO GET TOUCHSCRIPT WORKING** //
+        map.InputDelegate += UnitySlippyMap.Input.MapInput.BasicTouchAndKeyboard;
+
+		map.CurrentZoom = 18.0f;
+		// UVic
+
+		map.CenterWGS84 = new double[2] { -123.310900, 48.460959 };
+		map.UsesLocation = true;
+		map.InputsEnabled = true;
+		map.ShowsGUIControls = true;
+
+		map.GUIDelegate += Toolbar;
+
+        layers = new List<LayerBehaviour>();
+
+		// create an Esri tile layer
+        EsriTileLayer esriLayer = map.CreateLayer<EsriTileLayer>("Esri");
+        esriLayer.BaseURL = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/";
+		
+        layers.Add(esriLayer);
+
+        yield return null;
+	
+	}
+	
+	void OnApplicationQuit()
+	{
+		map = null;
+	}
+	
+	void Update()
+	{
+		if (destinationAngle != 0.0f)
+		{
+            // changing from 2D to 3D ortho angle
+
+            Vector3 cameraLeft = Quaternion.AngleAxis(-90.0f, Camera.main.transform.up) * Camera.main.transform.forward;
+			if ((Time.time - animationStartTime) < animationDuration)
+			{
+				float angle = Mathf.LerpAngle(0.0f, destinationAngle, (Time.time - animationStartTime) / animationDuration);
+				Camera.main.transform.RotateAround(Vector3.zero, cameraLeft, angle - currentAngle);
+				currentAngle = angle;
+			}
+			else
+			{
+				Camera.main.transform.RotateAround(Vector3.zero, cameraLeft, destinationAngle - currentAngle);
+				destinationAngle = 0.0f;
+				currentAngle = 0.0f;
+				map.IsDirty = true;
+			}
+			
+			map.HasMoved = true;
+		}
+
+	}
+	
+#if DEBUG_PROFILE
+	void LateUpdate()
+	{
+		Debug.Log("PROFILE:\n" + UnitySlippyMap.Profiler.Dump());
+		UnitySlippyMap.Profiler.Reset();
+	}
+#endif
+}
+
---
+++ Assets/UnitySlippyMap/Layers/EsriTileLayerBehaviour.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.IO;
+
+using UnityEngine;
+
+using UnitySlippyMap.Helpers;
+
+namespace UnitySlippyMap.Layers
+{
+    public class EsriTileLayer : WebTileLayerBehaviour
+    {
+        #region Private members & properties
+
+
+        /// <summary>
+        /// The format for the URL parameters as in String.Format().
+        /// </summary>
+        /// https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}
+        /// World_Street_Map
+        /// 
+
+        private string urlParametersFormat = "{0}/{2}/{1}";
+
+        /// <summary>
+        /// Gets or sets the URL parameters format.
+        /// </summary>
+        /// <value>The format for the URL parameters as in String.Format().</value>
+        public string URLParametersFormat
+        {
+            //returns the private global string
+            get { return urlParametersFormat; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+                if (value == String.Empty)
+                    throw new ArgumentException("value cannot be empty");
+                urlParametersFormat = value;
+            }
+        }
+
+        /// <summary>
+        /// The extension of the tile files.
+        /// </summary>
+        private string tileImageExtension = ".png";
+
+        /// <summary>
+        /// Gets or sets the tile image extension.
+        /// </summary>
+        /// <value>The extension of the tile files.</value>
+        public string TileImageExtension
+        {
+            get { return tileImageExtension; }
+            set
+            {
+                tileImageExtension = value;
+                if (tileImageExtension == null)
+                    tileImageExtension = String.Empty;
+            }
+        }
+
+        #endregion
+
+        #region EsriTileLayer implementation
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnitySlippyMap.Layers.EsriTileLayer"/> class.
+        /// </summary>
+        public EsriTileLayer()
+        {
+            isReadyToBeQueried = true;
+        }
+
+        #endregion
+
+        #region MonoBehaviour implementation
+
+        /// <summary>
+        /// Implementation of <see cref="http://docs.unity3d.com/ScriptReference/MonoBehaviour.html">MonoBehaviour</see>.Awake().
+        /// </summary>
+        private new void Awake()
+        {
+            base.Awake();
+            minZoom = 1;
+            maxZoom = 19;
+        }
+
+        #endregion
+
+        #region TileLayer implementation
+
+        /// <summary>
+        /// Gets the tile count per axis. See <see cref="UnitySlippyMap.Layers.TileLayerBehaviour.GetTileCountPerAxis"/>.
+        /// </summary>
+        /// <param name="tileCountOnX">Tile count on x.</param>
+        /// <param name="tileCountOnY">Tile count on y.</param>
+        protected override void GetTileCountPerAxis(out int tileCountOnX, out int tileCountOnY)
+        {
+            tileCountOnX = tileCountOnY = (int)Mathf.Pow(2, Map.RoundedZoom);
+        }
+
+        /// <summary>
+        /// Gets the center tile. See <see cref="UnitySlippyMap.Layers.TileLayerBehaviour.GetCenterTile"/>.
+        /// </summary>
+        /// <param name="tileCountOnX">Tile count on x.</param>
+        /// <param name="tileCountOnY">Tile count on y.</param>
+        /// <param name="tileX">Tile x.</param>
+        /// <param name="tileY">Tile y.</param>
+        /// <param name="offsetX">Offset x.</param>
+        /// <param name="offsetZ">Offset z.</param>
+        protected override void GetCenterTile(int tileCountOnX, int tileCountOnY, out int tileX, out int tileY, out float offsetX, out float offsetZ)
+        {
+            int[] tileCoordinates = GeoHelpers.WGS84ToTile(Map.CenterWGS84[0], Map.CenterWGS84[1], Map.RoundedZoom);
+            double[] centerTile = GeoHelpers.TileToWGS84(tileCoordinates[0], tileCoordinates[1], Map.RoundedZoom);
+            double[] centerTileMeters = Map.WGS84ToEPSG900913Transform.Transform(centerTile); //GeoHelpers.WGS84ToMeters(centerTile[0], centerTile[1]);
+
+            tileX = tileCoordinates[0];
+            tileY = tileCoordinates[1];
+            offsetX = Map.RoundedHalfMapScale / 2.0f - (float)(Map.CenterEPSG900913[0] - centerTileMeters[0]) * Map.RoundedScaleMultiplier;
+            offsetZ = -Map.RoundedHalfMapScale / 2.0f - (float)(Map.CenterEPSG900913[1] - centerTileMeters[1]) * Map.RoundedScaleMultiplier;
+        }
+
+        /// <summary>
+        /// Gets a neighbour tile. See <see cref="UnitySlippyMap.Layers.TileLayerBehaviour.GetNeighbourTile"/>.
+        /// </summary>
+        /// <returns><c>true</c>, if neighbour tile was gotten, <c>false</c> otherwise.</returns>
+        /// <param name="tileX">Tile x.</param>
+        /// <param name="tileY">Tile y.</param>
+        /// <param name="offsetX">Offset x.</param>
+        /// <param name="offsetZ">Offset z.</param>
+        /// <param name="tileCountOnX">Tile count on x.</param>
+        /// <param name="tileCountOnY">Tile count on y.</param>
+        /// <param name="dir">Dir.</param>
+        /// <param name="nTileX">N tile x.</param>
+        /// <param name="nTileY">N tile y.</param>
+        /// <param name="nOffsetX">N offset x.</param>
+        /// <param name="nOffsetZ">N offset z.</param>
+        protected override bool GetNeighbourTile(int tileX, int tileY, float offsetX, float offsetZ, int tileCountOnX, int tileCountOnY, NeighbourTileDirection dir, out int nTileX, out int nTileY, out float nOffsetX, out float nOffsetZ)
+        {
+            bool ret = false;
+            nTileX = 0;
+            nTileY = 0;
+            nOffsetX = 0.0f;
+            nOffsetZ = 0.0f;
+
+            switch (dir)
+            {
+                case NeighbourTileDirection.South:
+                    if ((tileY + 1) < tileCountOnY)
+                    {
+                        nTileX = tileX;
+                        nTileY = tileY + 1;
+                        nOffsetX = offsetX;
+                        nOffsetZ = offsetZ - Map.RoundedHalfMapScale;
+                        ret = true;
+                    }
+                    break;
+
+                case NeighbourTileDirection.North:
+                    if (tileY > 0)
+                    {
+                        nTileX = tileX;
+                        nTileY = tileY - 1;
+                        nOffsetX = offsetX;
+                        nOffsetZ = offsetZ + Map.RoundedHalfMapScale;
+                        ret = true;
+                    }
+                    break;
+
+                case NeighbourTileDirection.East:
+                    nTileX = tileX + 1;
+                    nTileY = tileY;
+                    nOffsetX = offsetX + Map.RoundedHalfMapScale;
+                    nOffsetZ = offsetZ;
+                    ret = true;
+                    break;
+
+                case NeighbourTileDirection.West:
+                    nTileX = tileX - 1;
+                    nTileY = tileY;
+                    nOffsetX = offsetX - Map.RoundedHalfMapScale;
+                    nOffsetZ = offsetZ;
+                    ret = true;
+                    break;
+            }
+
+
+            return ret;
+        }
+
+        #endregion
+
+        #region WebTileLayer implementation
+
+        /// <summary>
+        /// Gets a tile URL. See <see cref="UnitySlippyMap.Layers.TileLayerBehaviour.GetTileURL"/>.
+        /// </summary>
+        /// <returns>The tile URL.</returns>
+        /// <param name="tileX">Tile x.</param>
+        /// <param name="tileY">Tile y.</param>
+        /// <param name="roundedZoom">Rounded zoom.</param>
+        protected override string GetTileURL(int tileX, int tileY, int roundedZoom)
+        {
+            return String.Format(Path.Combine(BaseURL, URLParametersFormat).Replace("\\", "/") + TileImageExtension, roundedZoom, tileX, tileY);
+        }
+
+        #endregion
+    }
+}
+
+
